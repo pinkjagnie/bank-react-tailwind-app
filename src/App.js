3efdb8aa26ba33466b8ad49061c229efee0ca635
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-07 22:42:52 +0100

    Setup layout with blank components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
 import styles from './style'
 
 const App = () => {
@@ -7,26 +9,26 @@ const App = () => {
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          stats
-          buisiness
-          billing
-          card deal 
-          testimonials
-          clients
-          cta
-          footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal /> 
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
---
+++ src/components/CardDeal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardDeal = () => {
+  return(
+    <div>
+      card deal
+    </div>
+  )
+};
+
+export default CardDeal;
